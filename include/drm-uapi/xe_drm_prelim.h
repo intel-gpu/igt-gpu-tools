ab0e431069792d9fd8a53cfd29aadf479ab3f03f
Pravalika Gurram <pravalika.gurram@intel.com>
2026-04-10 12:20:22 +0530

    drm-uapi: Align uapi headers with v6.17 Xekmd-backport
    Update 6.17 drm-uapi headers to align with latest v6.17 kernel,
    including prelim eudebug macro updates for debug interface support.
    
    Updated eudebug macros for v6.17 compatibility
    
    Signed-off-by: Pravalika Gurram <pravalika.gurram@intel.com>

--- include/drm-uapi/xe_drm_prelim.h
+++ include/drm-uapi/xe_drm_prelim.h
@@ -66,6 +66,24 @@
  */
 
 /*
+ * Default marker for PRELIM values in shared flags/extensions
+ */
+#define PRELIM_DRM_XE_VALUE_INDICATOR		(1 << 16)
+
+/*
+ * Default mask for PRELIM values created by PRELIM_DRM_XE_VALUE_INDICATOR.
+ * The PRELIM value shall be created by:
+ * PRELIM_DRM_XE_VALUE_INDICATOR | value
+ * and 'value' shall be smaller than MASK value.
+ */
+#define PRELIM_DRM_XE_VALUE_MASK		0xffff
+
+/*
+ * Value of the PRELIM flag/extension without PRELIM INDICATOR
+ */
+#define PRELIM_DRM_XE_VALUE(val)		((val) & PRELIM_DRM_XE_VALUE_MASK)
+
+/*
  * IOCTL numbers listed below are reserved, they are taken up by other
  * components. Please add an unreserved ioctl number here to reserve that
  * number.
@@ -75,9 +93,9 @@
  * Copyright © 2023 Intel Corporation
  */
 
-#define PRELIM_DRM_XE_EUDEBUG_CONNECT 0x5f
-#define PRELIM_DRM_XE_DEBUG_METADATA_CREATE 0x5e
-#define PRELIM_DRM_XE_DEBUG_METADATA_DESTROY 0x5d
+#define PRELIM_DRM_XE_EUDEBUG_CONNECT 0x59
+#define PRELIM_DRM_XE_DEBUG_METADATA_CREATE 0x5b
+#define PRELIM_DRM_XE_DEBUG_METADATA_DESTROY 0x5c
 
 #define PRELIM_DRM_IOCTL_XE_EUDEBUG_CONNECT		DRM_IOWR(DRM_COMMAND_BASE + PRELIM_DRM_XE_EUDEBUG_CONNECT, struct prelim_drm_xe_eudebug_connect)
 #define PRELIM_DRM_IOCTL_XE_DEBUG_METADATA_CREATE	 DRM_IOWR(DRM_COMMAND_BASE + PRELIM_DRM_XE_DEBUG_METADATA_CREATE, struct prelim_drm_xe_debug_metadata_create)
@@ -100,9 +118,9 @@ struct prelim_drm_xe_vm_bind_op_ext_attach_debug {
 	__u64 reserved;
 };
 
-#define PRELIM_XE_VM_BIND_OP_EXTENSIONS_ATTACH_DEBUG 0
+#define PRELIM_XE_VM_BIND_OP_EXTENSIONS_ATTACH_DEBUG (PRELIM_DRM_XE_VALUE_INDICATOR)
 
-#define   PRELIM_DRM_XE_EXEC_QUEUE_SET_PROPERTY_EUDEBUG		2
+#define   PRELIM_DRM_XE_EXEC_QUEUE_SET_PROPERTY_EUDEBUG		(PRELIM_DRM_XE_VALUE_INDICATOR)
 #define     PRELIM_DRM_XE_EXEC_QUEUE_EUDEBUG_FLAG_ENABLE		(1 << 0)
 
 /*
@@ -123,7 +141,7 @@ struct prelim_drm_xe_eudebug_connect {
 };
 
 /*
- * struct prelim_drm_xe_debug_metadata_create - Create debug metadata
+ * struct drm_xe_debug_metadata_create - Create debug metadata
  *
  * Add a region of user memory to be marked as debug metadata.
  * When the debugger attaches, the metadata regions will be delivered
@@ -158,7 +176,7 @@ struct prelim_drm_xe_debug_metadata_create {
 };
 
 /**
- * struct prelim_drm_xe_debug_metadata_destroy - Destroy debug metadata
+ * struct drm_xe_debug_metadata_destroy - Destroy debug metadata
  *
  * Destroy debug metadata.
  */
@@ -169,8 +187,6 @@ struct prelim_drm_xe_debug_metadata_destroy {
 	/** @metadata_id: metadata handle to destroy */
 	__u32 metadata_id;
 };
-
-#include "xe_drm_prelim.h"
 
 /**
  * Do a eudebug event read for a debugger connection.
@@ -318,7 +334,7 @@ struct prelim_drm_xe_eudebug_eu_control {
  * Client's UFENCE sync will be held by the driver: client's
  * drm_xe_wait_ufence will not complete and the value of the ufence
  * won't appear until ufence is acked by the debugger process calling
- * PRELIM_DRM_XE_EUDEBUG_IOCTL_ACK_EVENT with the event_ufence.base.seqno.
+ * DRM_XE_EUDEBUG_IOCTL_ACK_EVENT with the event_ufence.base.seqno.
  * This will signal the fence, .value will update and the wait will
  * complete allowing the client to continue.
  *
@@ -369,7 +385,7 @@ struct prelim_drm_xe_eudebug_vm_open {
 	/** @flags: flags */
 	__u64 flags;
 
-#define DRM_XE_EUDEBUG_VM_SYNC_MAX_TIMEOUT_NSECS (10ULL * NSEC_PER_SEC)
+#define PRELIM_DRM_XE_EUDEBUG_VM_SYNC_MAX_TIMEOUT_NSECS (10ULL * NSEC_PER_SEC)
 	/** @timeout_ns: Timeout value in nanoseconds operations (fsync) */
 	__u64 timeout_ns;
 };
